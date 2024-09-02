91aa58d64a77e0dce47089eb2439b9511ad2d10f
perpel <572821520@qq.com>
2024-09-02 19:48:28 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/kordar/gorbac-starter
+
+go 1.18
+
+require (
+	github.com/kordar/goframework-goredis v0.0.1
+	github.com/kordar/goframework-gorm-mysql v1.0.10
+	github.com/kordar/gologger v0.0.8
+	github.com/kordar/gorbac v1.0.8
+	github.com/kordar/gorbac-gorm v0.0.2
+	github.com/kordar/gorbac-redis v0.0.1
+	github.com/spf13/cast v1.7.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kordar/godb v0.0.7 // indirect
+	github.com/redis/go-redis/v9 v9.6.1 // indirect
+	gorm.io/driver/mysql v1.5.7 // indirect
+	gorm.io/gorm v1.25.10 // indirect
+)
---
+++ starter.go
@@ -0,0 +1,68 @@
+package gorbac_starter
+
+import (
+	goframeworkgoredis "github.com/kordar/goframework-goredis"
+	goframeworkgormmysql "github.com/kordar/goframework-gorm-mysql"
+	logger "github.com/kordar/gologger"
+	"github.com/kordar/gorbac"
+	"github.com/kordar/gorbac-gorm"
+	"github.com/kordar/gorbac-redis"
+	"github.com/spf13/cast"
+)
+
+func getMapStr(m map[string]interface{}, field string, value string) string {
+	if m[field] == "" {
+		return value
+	} else {
+		return cast.ToString(m[field])
+	}
+}
+
+type RbacModule struct {
+}
+
+func (m RbacModule) Name() string {
+	return "rbac_starter"
+}
+
+func (m RbacModule) Load(value interface{}) {
+	cfg := cast.ToStringMap(value)
+	gorbac.SetTableName("rule", getMapStr(cfg, "t_rule", "sys_auth_rule"))
+	gorbac.SetTableName("item", getMapStr(cfg, "t_item", "sys_auth_item"))
+	gorbac.SetTableName("item-child", getMapStr(cfg, "t_item_child", "sys_auth_item_child"))
+	gorbac.SetTableName("assignment", getMapStr(cfg, "t_assignment", "sys_auth_assignment"))
+
+	driver := getMapStr(cfg, "driver", "mysql")
+
+	var repos gorbac.AuthRepository
+	db := getMapStr(cfg, "db", "gorbac")
+	if driver == "mysql" {
+		if !goframeworkgormmysql.HasMysqlInstance(db) {
+			logger.Warnf("初始化rbac组件失败，请先初始化数据库%s", db)
+			return
+		}
+
+		mysqlDB := goframeworkgormmysql.GetMysqlDB(db)
+		repos = gorbac_gorm.NewSqlRbac(mysqlDB)
+	}
+
+	if driver == "redis" {
+		if !goframeworkgoredis.HasRedisInstance(db) {
+			logger.Warnf("初始化rbac组件失败，请先初始化数据库%s", db)
+			return
+		}
+		tb := getMapStr(cfg, "table", "gorbac_table")
+		mysqlDB := goframeworkgoredis.GetRedisClient(db)
+		repos = gorbac_redis.NewRedisRbac(mysqlDB, tb)
+	}
+
+	cache := cast.ToBool(cfg["cache"])
+	rbacManager := gorbac.NewDefaultManager(repos, cache)
+
+	guest := getMapStr(cfg, "guest", "guest")
+	role := rbacManager.CreateRole(guest)
+	rbacManager.SetDefaultRoles(role)
+}
+
+func (m RbacModule) Close() {
+}
